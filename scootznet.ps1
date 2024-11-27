3c066cacf056457e1547f55bc814b6db4452843b
scooter <yalbik@yahoo.com>
2024-11-26 17:50:10 -0800

    clean up inputs, outputs, and console output

--- scootznet.ps1
+++ scootznet.ps1
@@ -19,7 +19,7 @@ foreach ($File in $Library)
 }
 
 $NETWORK_LAYERS = @(8, 16, 8)
-$RANDOM_DATA_SIZE = 10
+$RANDOM_DATA_SIZE = 16
 $TRAINING_PASS_EPOCHS = 1000
 $TRAINING_PASSES = 10
 
@@ -42,10 +42,12 @@ Function Random-SevenBitNumber
 Write-Host "Creating a neural network with $($NETWORK_LAYERS.Length) layers: $($NETWORK_LAYERS -join '-')..."
 $MyNetwork = [NeuralNetwork]::new($NETWORK_LAYERS)
 
+Write-Host "Training the network with $($TRAINING_PASSES) passes of $($TRAINING_PASS_EPOCHS) epochs each, $($RANDOM_DATA_SIZE) inputs per pass..."
+
 $Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
 for ($Pass = 0; $Pass -lt $TRAINING_PASSES; $Pass++)
 {
-    Write-Host "Training pass $($Pass + 1) of $($TRAINING_PASSES)..."
+    Write-Host "Training pass $($Pass + 1) of $($TRAINING_PASSES) ($($Stopwatch.Elapsed.TotalSeconds)s elapsed)..."
     $inputsBase = @()
     while (([array]$inputsBase).Length -lt $RANDOM_DATA_SIZE)
     { 
@@ -66,18 +68,32 @@ for ($Pass = 0; $Pass -lt $TRAINING_PASSES; $Pass++)
 $Stopwatch.Stop()
 Write-Host "Training completed in $($Stopwatch.Elapsed.TotalSeconds) seconds"
 
-$testInputs = @(
-    @(0, 0, 0, 0, 0, 0, 0, 0),
-    @(0, 0, 0, 0, 0, 0, 0, 1),
-    @(0, 0, 0, 0, 0, 0, 1, 0),
-    @(0, 0, 0, 0, 0, 1, 0, 0),
-    @(0, 0, 0, 0, 1, 0, 0, 0),
-    @(0, 0, 0, 1, 0, 0, 0, 0),
-    @(0, 0, 1, 0, 0, 0, 0, 0),
-    @(0, 1, 0, 0, 0, 0, 0, 0)
+$testInputsInt = @(
+    0,
+    1,
+    2,
+    4,
+    8,
+    16,
+    32,
+    64
 )
 
-foreach ($input in $testInputs) {
+$expectedOutputsInt = $testInputsInt | ForEach-Object { [int]($_ * 2) }
+
+$testInputs = $testInputsInt | ForEach-Object { ,(To-Binary $_) }
+$expectedOutputs = $expectedOutputsInt | ForEach-Object { ,(To-Binary $_) }
+
+for ($i = 0; $i -lt $testInputs.Length; $i++)
+{
+    $inputInt = $testInputsInt[$i]
+    $input = $testInputs[$i]
+    $expectedInt = $expectedOutputsInt[$i]
+    $expected = $expectedOutputs[$i]
+
     $output = $MyNetwork.Predict(@($input))
-    Write-Host "Input: $input, Output: $output"
+    
+    Write-Host "Input: ($($inputInt)): $($input)"
+    Write-Host "`tExpected: ($($expectedInt)): $($expected)"
+    Write-Host "`tPredicted: $output"
 }
